387e64a3bb80d97efb89ff4549351a884abd4d41
Diogo-Mendes-Martins <81984611+Diogo-Mendes-Martins@users.noreply.github.com>
2025-12-16 00:33:21 -0300

    Add files via upload

---
+++ Exercícios/Ex3.java
@@ -0,0 +1,24 @@
+package Exercícios;
+
+public class Ex3 {
+
+	public static void main(String[] args) {
+		
+		int[] numeros = {10, 20, 30, 40};
+
+		
+		int soma = 0;
+
+		for (int i = 0; i < numeros.length; i++) {
+		    soma += numeros[i];
+		}
+
+		System.out.println(soma);
+
+
+	}
+
+}
+
+
+/*Dado um array de números, calcule a soma total dos valores*/
